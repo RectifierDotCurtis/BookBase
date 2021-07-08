5345f3be7332b9b36bed4c2f955ce7c008a2a6c6
RectifierDotErik <86714198+RectifierDotErik@users.noreply.github.com>
2021-07-08 12:22:13 -0400

    Flowchart Start
    Created placeholder question under two buttons that are answers to that question. Moved navigation buttons down slightly.

--- screens/Dashboard.js
+++ screens/Dashboard.js
@@ -1,90 +1,137 @@
-import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet, Alert} from 'react-native';
-import { TouchableOpacity } from 'react-native-gesture-handler';
-import * as firebase from 'firebase';
-import {loggingOut} from '../API/firebaseMethods';
+import React, { useEffect, useState } from "react";
+import { View, Text, StyleSheet, Alert } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import * as firebase from "firebase";
+import { loggingOut } from "../API/firebaseMethods";
 
 export default function Dashboard({ navigation }) {
   let currentUserUID = firebase.auth().currentUser.uid;
-  const [firstName, setFirstName] = useState('');
+  const [firstName, setFirstName] = useState("");
 
   useEffect(() => {
-    async function getUserInfo(){
+    async function getUserInfo() {
       try {
         let doc = await firebase
           .firestore()
-          .collection('users')
+          .collection("users")
           .doc(currentUserUID)
           .get();
 
-        if (!doc.exists){
-          Alert.alert('No user data found!')
+        if (!doc.exists) {
+          Alert.alert("No user data found!");
         } else {
           let dataObj = doc.data();
-          setFirstName(dataObj.firstName)
+          setFirstName(dataObj.firstName);
         }
-      } catch (err){
-      Alert.alert('There is an error.', err.message)
+      } catch (err) {
+        Alert.alert("There is an error.", err.message);
       }
     }
     getUserInfo();
-  })
+  });
 
   const handlePress = () => {
     loggingOut();
-    navigation.replace('Home');
+    navigation.replace("Home");
   };
 
   return (
     <View style={styles.container}>
       <Text style={styles.titleText}>Dashboard</Text>
-      <Text style={styles.text}>Hi {firstName}</Text>
-      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('GuestBook')} >
-        <Text style={styles.buttonText}>GuestBook</Text>
-       </TouchableOpacity>
-      <TouchableOpacity style={styles.button} onPress={handlePress}>
-        <Text style={styles.buttonText}>Log Out</Text>
-      </TouchableOpacity>
+      {/* <Text style={styles.text}>Hi {firstName}</Text> */}
+      <Text style={( styles.questionText)}>
+        {" "}
+        No laboramus maiestatis pri, ex vis rebum doctus?
+      </Text>
+      <View style={styles.answerButtonContainer}>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Answer 1</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Answer 2</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.navContainer}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.navigate("GuestBook")}
+        >
+          <Text style={styles.buttonText}>GuestBook</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={handlePress}>
+          <Text style={styles.buttonText}>Log Out</Text>
+        </TouchableOpacity>
+      </View>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    height: "100%",
+    width: "100%",
+    backgroundColor: "#3FC5AB",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
   button: {
     width: 150,
-    padding: 5,
-    backgroundColor: '#ff9999',
+    padding: 8,
+    backgroundColor: "#ff9999",
     borderWidth: 2,
-    borderColor: 'white',
+    borderColor: "white",
     borderRadius: 15,
-    alignSelf: 'center',
+    alignSelf: "center",
+    marginTop: 5,
+    marginHorizontal: 8,
   },
+
   buttonText: {
-    fontSize:20,
-    color: 'white',
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-  container: {
-    height: '100%',
-    width: '100%',
-    backgroundColor: '#3FC5AB',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    textAlign: 'center',
     fontSize: 20,
-    fontStyle: 'italic',
-    marginTop: '2%',
-    marginBottom: '10%',
-    fontWeight: 'bold',
-    color: 'black',
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
   },
+
+  answerButtonContainer: {
+    flexDirection: "row",
+    position: "relative",
+    bottom: "10%",
+  },
+
+  navContainer: {
+    top: "30%",
+    position: "relative",
+  },
+
+  text: {
+    textAlign: "center",
+    fontSize: 20,
+    fontStyle: "italic",
+    marginTop: "2%",
+    marginBottom: "10%",
+    fontWeight: "bold",
+    color: "black",
+  },
+
   titleText: {
-    textAlign: 'center',
+    bottom: "25%",
+    position: "relative",
+    textAlign: "center",
     fontSize: 30,
-    fontWeight: 'bold',
-    color: '#2E6194',
+    fontWeight: "bold",
+    color: "#2E6194",
+  },
+
+  questionText: {
+    color: "black",
+    bottom: "10%",
+    position: "relative",
+    fontSize: 22,
+    textAlign: "center",
+    fontWeight: "bold",
+    marginBottom: "5%",
   },
 });
